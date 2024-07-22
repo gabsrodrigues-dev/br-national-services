3cf58546e7553a7c06ca67f7525cc8de638d1509
gabrielrobrigues <gabrielrod.ifmg@gmail.com>
2024-07-22 16:53:54 -0300

    Final Basic Services

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,15 @@ declare module 'br-national-services' {
   function validateEmail(email: string): boolean;
   function formatToCurrency(number: number): string;
   function parseCurrencyToNumber(currency: string): number;
+  function formatPhoneNumber(number: string): string;
+  function parsePhoneNumber(formatted: string): string;
 
   export { 
     verifyCPF, 
     validateEmail,
     formatToCurrency,
-    parseCurrencyToNumber
+    parseCurrencyToNumber,
+    formatPhoneNumber,
+    parsePhoneNumber
   };
 }

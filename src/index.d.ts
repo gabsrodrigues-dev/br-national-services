8c370987b35271a7ad3455887a58b9092cca3b65
gabrielrobrigues <gabrielrod.ifmg@gmail.com>
2024-07-23 13:47:28 -0300

    Update NPM

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  interface String {
+    brFormat(type: 'cpf' | 'phone' | 'currency'): string;
+    brValidate(type: 'cpf' | 'email'): boolean;
+    brParse(type: 'phone' | 'currency'): number;
+  }
+
+  interface Number {
+    brFormat(type: 'currency'): string;
+  }
+}
+
+export {};
